3ab9a1ac9bd4dc4137dd736df366976208569c28
Hey <h1017760@connect.hku.hk>
2015-09-06 01:14:06 +0800

    Fixed intellij misc.xml and compiler.xml error, added 1 test

--- java/src/main/java/lambda/practice/one/Practice.java
+++ java/src/main/java/lambda/practice/one/Practice.java
@@ -1,12 +1,17 @@
 package lambda.practice.one;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
  * Created by Hei on 03/09/2015.
- *
+ * <p>
  * ------------------------------------
- *
+ * <p>
  * A practice for java 8 lambda expression
  */
 public class Practice {
@@ -14,5 +19,9 @@ public class Practice {
         String a = "a";
         Function<String, String> b = s -> s;
         return b.apply(a);
+    }
+
+    public Map transformList(List<Object> list) {
+        return Maps.asMap(Sets.newHashSet(list), o -> o);
     }
 }
--- java/src/test/java/lambda/practice/one/PracticeTest.java
+++ java/src/test/java/lambda/practice/one/PracticeTest.java
@@ -1,9 +1,14 @@
 package lambda.practice.one;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,8 +29,10 @@ public class PracticeTest {
 
     @Test
     public void shouldTransformListToMap() throws Exception {
+        Map map = ImmutableMap.of(1, "a", 2, "b");
+        List<Object> list = Lists.newArrayList("a", "b");
 
-//        assertEquals();
+        assertEquals(map, practice.transformList(list));
 
     }
 }
