e328496c62535db2c13c9b56656e9b4059ed0f92
Hey <h1017760@connect.hku.hk>
2015-12-22 00:47:43 +0800

    leetCode, added 1 implementation from internet for Single Number

--- java/src/main/java/online/leetcode/SingleNumber.java
+++ java/src/main/java/online/leetcode/SingleNumber.java
@@ -19,18 +19,29 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-//        According to assumption, appears TWICE -> no 3 times, so can use set
-        final HashSet<Integer> set = new HashSet<>();
-
+//        http://www.programcreek.com/2012/12/leetcode-solution-of-single-number-in-java/
+        int returnValue = 0;
         for (final int value : nums) {
-            final boolean valueInSet = set.contains(value);
-            if (!valueInSet) {
-                set.add(value);
-            } else {
-                set.remove(value);
-            }
+            returnValue ^= value;
         }
+        return returnValue;
+    }
 
-        return set.iterator().next();
+    private class MyAttemptUsingHashSet {
+        public int singleNumber(int[] nums) {
+//        According to assumption, appears TWICE -> no 3 times, so can use set
+            final HashSet<Integer> set = new HashSet<>();
+
+            for (final int value : nums) {
+                final boolean valueInSet = set.contains(value);
+                if (!valueInSet) {
+                    set.add(value);
+                } else {
+                    set.remove(value);
+                }
+            }
+
+            return set.iterator().next();
+        }
     }
 }
