d367b9ecbe6405b01c2d04efb92b67f940f31c88
Hey <h1017760@connect.hku.hk>
2015-12-27 12:45:54 +0800

    leetCode, solved 1 (BinaryTreePreorderTraversal) in both iterative and recursive approaches

---
+++ java/src/main/java/online/leetcode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,101 @@
+package online.leetcode;
+
+/**
+ * Created by Hey on 19/12/15
+ */
+
+/*
+
+https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+Subscribe to see which companies asked this question
+
+*/
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+// [6,2,7,1,4,null,9,null,null,3,5,8]
+
+public class BinaryTreePreorderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        TreeNode current = root;
+
+        if (current == null) {
+            return result;
+        }
+
+        stack.push(current);
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+            result.add(current.val);
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return result;
+    }
+
+    private class RecursionApproachPreOrder {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+
+            if (root == null) {
+                return result;
+            }
+
+            result.add(root.val);
+            result.addAll(preorderTraversal(root.left));
+            result.addAll(preorderTraversal(root.right));
+
+            return result;
+        }
+    }
+
+}
