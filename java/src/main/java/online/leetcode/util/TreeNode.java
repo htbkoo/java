0fa4896e9d8a4c7a989d4cd20fcd8f8df0859026
Hey <h1017760@connect.hku.hk>
2016-01-08 19:25:00 +0800

    leetCode, created TreeNode class as infrastructure use

---
+++ java/src/main/java/online/leetcode/util/TreeNode.java
@@ -0,0 +1,18 @@
+package online.leetcode.util;
+
+/**
+ * Created by Hey on 8/1/16
+ *
+ * ---
+ *
+ * For LeetCode TreeNode type
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
