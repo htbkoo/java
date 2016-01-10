df06fc8ee239226312befdf0d0462fa8203b212e
Hey <h1017760@connect.hku.hk>
2016-01-11 00:00:54 +0800

    leetCode, implementing 1 (MinimumDepthOfBinaryTree [array approach])

--- java/src/main/java/online/leetcode/MinimumDepthOfBinaryTree.java
+++ java/src/main/java/online/leetcode/MinimumDepthOfBinaryTree.java
@@ -33,26 +33,61 @@ public class MinimumDepthOfBinaryTree {
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         int height = 0;
-
-        List<TreeNode> thisLevel;
-        List<TreeNode> nextLevel = new ArrayList<>();
-        nextLevel.add(root);
-        while (!nextLevel.isEmpty()) {
+        int numChildren = 0;
+        TreeNode[] thisLevel;
+        TreeNode[] nextLevel = new TreeNode[2*height+1];
+        nextLevel[numChildren] = root;
+        ++numChildren;
+        while (numChildren>0) {
             thisLevel = nextLevel;
             ++height;
-            nextLevel = new ArrayList<>();
+            numChildren = 0;
+            nextLevel = new TreeNode[2*height+1];
             for (TreeNode node : thisLevel) {
+                if (node==null){
+                    break;
+                }
                 if ((node.left == null) && (node.right == null)) {
                     return height;
                 }
                 if (node.left != null) {
-                    nextLevel.add(node.left);
+                    nextLevel[numChildren] = node.left;
+                    ++numChildren;
                 }
                 if (node.right != null) {
-                    nextLevel.add(node.right);
+                    nextLevel[numChildren] = node.right;
+                    ++numChildren;
                 }
             }
         }
         return height;
+    }
+
+    private class SlowUsingObjectADT {
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            int height = 0;
+
+            List<TreeNode> thisLevel;
+            List<TreeNode> nextLevel = new ArrayList<>();
+            nextLevel.add(root);
+            while (!nextLevel.isEmpty()) {
+                thisLevel = nextLevel;
+                ++height;
+                nextLevel = new ArrayList<>();
+                for (TreeNode node : thisLevel) {
+                    if ((node.left == null) && (node.right == null)) {
+                        return height;
+                    }
+                    if (node.left != null) {
+                        nextLevel.add(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.add(node.right);
+                    }
+                }
+            }
+            return height;
+        }
     }
 }
