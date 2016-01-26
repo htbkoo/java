e1d2effb332a5dc40cd8eaf92efc137485180925
H.T. Koo <h1017760@connect.hku.hk>
2016-01-26 15:25:47 +0800

    leetcode, updated 1 (CoinChange [bottom-up with rolling array, 50ms])

--- java/src/main/java/online/leetcode/CoinChange.java
+++ java/src/main/java/online/leetcode/CoinChange.java
@@ -34,39 +34,79 @@ Subscribe to see which companies asked this question
 */
 
 public class CoinChange {
-    //    223ms, extremely slow
-    //    BFS with memoization
+    //    bottom-up approach
+    //    much faster than top-down, though still slow, 50 ms
     public int coinChange(int[] coins, int amount) {
         int count = 0;
         if (amount == 0) {
             return count;
         }
 
-        Set<Integer> set = new HashSet<>();
-        List<Integer> thisGen;
-        List<Integer> nextGen = new ArrayList<>();
-        set.add(amount);
-        nextGen.add(amount);
+        int arraySize = 0;
 
-        while (!nextGen.isEmpty()) {
-            ++count;
-            thisGen = nextGen;
-            nextGen = new ArrayList<>();
-            for (int i : thisGen) {
-                for (int c : coins) {
-                    final int remainingAmount = i - c;
-                    if (remainingAmount == 0) {
-                        return count;
-                    } else if (remainingAmount > 0) {
-                        if (!set.contains(remainingAmount)) {
-                            set.add(remainingAmount);
-                            nextGen.add(remainingAmount);
+        for (int c : coins) {
+            arraySize = Math.max(c, arraySize);
+        }
+
+        int[] minCoinNeeded = new int[arraySize + 1];
+//        Arrays.fill(minCoinNeeded, -1);
+        minCoinNeeded[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            final int curArrayPointer = i % (arraySize + 1);
+            minCoinNeeded[curArrayPointer] = -1;
+            for (int c : coins) {
+                final int prev = i - c;
+                if (prev >= 0) {
+                    final int prevArrayPointer = prev % (arraySize + 1);
+                    if (minCoinNeeded[prevArrayPointer] != -1) {
+                        if (minCoinNeeded[curArrayPointer] != -1) {
+                            minCoinNeeded[curArrayPointer] = Math.min(minCoinNeeded[prevArrayPointer] + 1, minCoinNeeded[curArrayPointer]);
+                        } else {
+                            minCoinNeeded[curArrayPointer] = minCoinNeeded[prevArrayPointer] + 1;
                         }
                     }
-
                 }
             }
         }
-        return -1;
+
+        return minCoinNeeded[amount % (arraySize + 1)];
+    }
+
+    private class TopDownBFSWithMemoizationApproach {
+        //    223ms, extremely slow
+        //    BFS with memoization
+        public int coinChange(int[] coins, int amount) {
+            int count = 0;
+            if (amount == 0) {
+                return count;
+            }
+
+            Set<Integer> set = new HashSet<>();
+            List<Integer> thisGen;
+            List<Integer> nextGen = new ArrayList<>();
+            set.add(amount);
+            nextGen.add(amount);
+
+            while (!nextGen.isEmpty()) {
+                ++count;
+                thisGen = nextGen;
+                nextGen = new ArrayList<>();
+                for (int i : thisGen) {
+                    for (int c : coins) {
+                        final int remainingAmount = i - c;
+                        if (remainingAmount == 0) {
+                            return count;
+                        } else if (remainingAmount > 0) {
+                            if (!set.contains(remainingAmount)) {
+                                set.add(remainingAmount);
+                                nextGen.add(remainingAmount);
+                            }
+                        }
+
+                    }
+                }
+            }
+            return -1;
+        }
     }
 }
