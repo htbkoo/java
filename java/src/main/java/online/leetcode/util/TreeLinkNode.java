e5aed35893c2b14c0448cfe99fca96e587ae06e5
H.T. Koo <h1017760@connect.hku.hk>
2016-03-25 02:11:46 +0800

    leetcode, updated infrastructure data type - TreeLinkNode

--- java/src/main/java/online/leetcode/util/TreeLinkNode.java
+++ java/src/main/java/online/leetcode/util/TreeLinkNode.java
@@ -11,8 +11,8 @@ package online.leetcode.util;
 // Definition for binary tree with next pointer.
 
 public class TreeLinkNode {
-    int val;
-    TreeLinkNode left, right, next;
+    public int val;
+    public TreeLinkNode left, right, next;
 
     TreeLinkNode(int x) {
         val = x;
