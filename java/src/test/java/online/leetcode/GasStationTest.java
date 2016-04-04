9162590675218464f26eeedd1d2b98546df22aca
H.T. Koo <h1017760@connect.hku.hk>
2016-04-04 08:54:23 +0800

    leetcode, minor updated 1 test (GasStation)

--- java/src/test/java/online/leetcode/GasStationTest.java
+++ java/src/test/java/online/leetcode/GasStationTest.java
@@ -30,7 +30,7 @@ public class GasStationTest {
         return Arrays.<Map<ParameterType, Object>[]>asList(
                 new Map[]{ImmutableMap.of(
                         ParameterType.GAS, new int[]{4},
-                        ParameterType.COST,new int[]{5},
+                        ParameterType.COST, new int[]{5},
                         ParameterType.ANSWER, -1
                 )},
                 new Map[]{ImmutableMap.of(
@@ -44,73 +44,73 @@ public class GasStationTest {
                         ParameterType.ANSWER, -1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,10},
-                        ParameterType.COST, new int[]{11,3},
+                        ParameterType.GAS, new int[]{0, 10},
+                        ParameterType.COST, new int[]{11, 3},
                         ParameterType.ANSWER, -1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,10},
-                        ParameterType.COST, new int[]{8,3},
+                        ParameterType.GAS, new int[]{0, 10},
+                        ParameterType.COST, new int[]{8, 3},
                         ParameterType.ANSWER, -1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,10},
-                        ParameterType.COST, new int[]{7,3},
+                        ParameterType.GAS, new int[]{0, 10},
+                        ParameterType.COST, new int[]{7, 3},
                         ParameterType.ANSWER, 1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{6,1,4,3,5},
-                        ParameterType.COST, new int[]{3,8,2,4,2},
+                        ParameterType.GAS, new int[]{6, 1, 4, 3, 5},
+                        ParameterType.COST, new int[]{3, 8, 2, 4, 2},
                         ParameterType.ANSWER, 2
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{3,5,6,1,4},
-                        ParameterType.COST, new int[]{4,2,3,8,2},
+                        ParameterType.GAS, new int[]{3, 5, 6, 1, 4},
+                        ParameterType.COST, new int[]{4, 2, 3, 8, 2},
                         ParameterType.ANSWER, 4
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{4,3,5,6,1},
-                        ParameterType.COST, new int[]{2,4,2,3,8},
+                        ParameterType.GAS, new int[]{4, 3, 5, 6, 1},
+                        ParameterType.COST, new int[]{2, 4, 2, 3, 8},
                         ParameterType.ANSWER, 0
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{1,2,3,4,5},
-                        ParameterType.COST, new int[]{3,4,5,1,2},
+                        ParameterType.GAS, new int[]{1, 2, 3, 4, 5},
+                        ParameterType.COST, new int[]{3, 4, 5, 1, 2},
                         ParameterType.ANSWER, 3
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{1,2,3,4,5},
-                        ParameterType.COST, new int[]{3,3,5,1,2},
+                        ParameterType.GAS, new int[]{1, 2, 3, 4, 5},
+                        ParameterType.COST, new int[]{3, 3, 5, 1, 2},
                         ParameterType.ANSWER, 3
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{2,3,1},
-                        ParameterType.COST, new int[]{3,1,2},
+                        ParameterType.GAS, new int[]{2, 3, 1},
+                        ParameterType.COST, new int[]{3, 1, 2},
                         ParameterType.ANSWER, 1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{2,4,1,0},
-                        ParameterType.COST, new int[]{3,1,2,0},
+                        ParameterType.GAS, new int[]{2, 4, 1, 0},
+                        ParameterType.COST, new int[]{3, 1, 2, 0},
                         ParameterType.ANSWER, 1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{2,4,0,1,0},
-                        ParameterType.COST, new int[]{3,1,0,2,0},
+                        ParameterType.GAS, new int[]{2, 4, 0, 1, 0},
+                        ParameterType.COST, new int[]{3, 1, 0, 2, 0},
                         ParameterType.ANSWER, 1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,2,2,2,2},
-                        ParameterType.COST, new int[]{4,1,1,1,1},
+                        ParameterType.GAS, new int[]{0, 2, 2, 2, 2},
+                        ParameterType.COST, new int[]{4, 1, 1, 1, 1},
                         ParameterType.ANSWER, 1
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,0,3,2,2,2},
-                        ParameterType.COST, new int[]{4,1,1,1,1,1},
+                        ParameterType.GAS, new int[]{0, 0, 3, 2, 2, 2},
+                        ParameterType.COST, new int[]{4, 1, 1, 1, 1, 1},
                         ParameterType.ANSWER, 2
                 )},
                 new Map[]{ImmutableMap.of(
-                        ParameterType.GAS, new int[]{0,10},
-                        ParameterType.COST,  new int[]{5,3},
+                        ParameterType.GAS, new int[]{0, 10},
+                        ParameterType.COST, new int[]{5, 3},
                         ParameterType.ANSWER, 1
                 )});
     }
