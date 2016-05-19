24c998dab8e7fd3986f611fa7c331907888e9a38
H.T. Koo <h1017760@connect.hku.hk>
2016-05-19 23:48:10 +0800

    Codewars - solved 1 (5Kyu - MaximumSubarraySum)

---
+++ java/src/test/java/online/codewars/_5kyu/MaximumSubarraySumTest.java
@@ -0,0 +1,20 @@
+package online.codewars._5kyu;
+
+import org.junit.Test;
+
+import static online.codewars._5kyu.MaximumSubarraySum.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Hey on 19 May 2016
+ */
+public class MaximumSubarraySumTest {
+    @Test
+    public void testEmptyArray() throws Exception {
+        assertEquals("Empty arrays should have a max of 0", 0, Max.sequence(new int[]{}));
+    }
+    @Test
+    public void testExampleArray() throws Exception {
+        assertEquals("Example array should have a max of 6", 6, Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
