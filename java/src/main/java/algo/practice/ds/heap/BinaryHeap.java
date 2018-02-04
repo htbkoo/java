60d42ce0f54820e1c495ed2bf9077a4691297276
H.T. Koo <h1017760@connect.hku.hk>
2018-02-04 16:32:59 +0800

    implemented heap sorting DataStructure

--- java/src/main/java/algo/practice/ds/heap/BinaryHeap.java
+++ java/src/main/java/algo/practice/ds/heap/BinaryHeap.java
@@ -8,6 +8,9 @@ public class BinaryHeap<T extends Comparable<? super T>> implements Heap<T> {
     @Override
     public void insert(T item) {
         list.add(item);
+        //        if (position > 0) {
+        heapify(getLastPosition());
+//        }
     }
 
     @Override
@@ -17,11 +20,78 @@ public class BinaryHeap<T extends Comparable<? super T>> implements Heap<T> {
 
     @Override
     public T pop() {
-        return list.remove(0);
+        final T topItem = list.get(0);
+
+        final int lastPosition = getLastPosition();
+
+        swap(0, lastPosition);
+        list.remove(lastPosition);
+
+        heapifyTopDown(0);
+
+        return topItem;
+    }
+
+    private void heapifyTopDown(int position) {
+        if (hasChild(position)) {
+            final int betterChildrenPosition = findBetterChildrenPosition(position);
+            if (isOrderWrong(betterChildrenPosition, position)){
+                swap(betterChildrenPosition, position);
+                heapifyTopDown(betterChildrenPosition);
+            }
+        }
+    }
+
+    private boolean hasChild(int position) {
+        return list.size() > (position * 2 + 1);
+    }
+
+    private int findBetterChildrenPosition(int position) {
+        final int leftPosition = position * 2 + 1, rightPosition = position * 2 + 2;
+        final boolean hasRightChild = list.size() > (position * 2 + 2);
+
+        if (!hasRightChild) {
+            return leftPosition;
+        } else {
+            final boolean isLeftSmaller = list.get(leftPosition).compareTo(list.get(rightPosition)) <= 0;
+            if (isLeftSmaller) {
+                return leftPosition;
+            } else {
+                return rightPosition;
+            }
+        }
     }
 
     @Override
     public int size() {
         return 0;
     }
+
+    private int getLastPosition() {
+        return list.size() - 1;
+    }
+
+    private void heapify(int position) {
+        int parentPosition = getParentPosition(position);
+        if (isOrderWrong(position, parentPosition)) {
+            swap(position, parentPosition);
+            heapify(parentPosition);
+        }
+    }
+
+    private int getParentPosition(int position) {
+        return (position - 1) / 2;
+    }
+
+    private boolean isOrderWrong(int position, int parentPosition) {
+        final T item = list.get(position), parent = list.get(parentPosition);
+        return item.compareTo(parent) < 0;
+    }
+
+    private void swap(int position, int parentPosition) {
+        final T item = list.get(position), parent = list.get(parentPosition);
+        list.set(position, parent);
+        list.set(parentPosition, item);
+    }
+
 }
--- java/src/test/java/algo/practice/ds/heap/BinaryHeapTest.java
+++ java/src/test/java/algo/practice/ds/heap/BinaryHeapTest.java
@@ -2,6 +2,9 @@ package algo.practice.ds.heap;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinaryHeapTest {
@@ -17,5 +20,19 @@ public class BinaryHeapTest {
         //        then
         Integer actual = integerHeap.pop();
         assertThat(actual).isEqualTo(givenItem);
+    }
+
+    @Test
+    public void should_be_able_to_insert_3_items_and_sort_and_pop_from_a_heap_of_integer() {
+        //        given
+        Heap<Integer> integerHeap = new BinaryHeap<>();
+        final List<Integer> givenItems = Arrays.asList(3, 4, 2);
+
+        //        when
+        givenItems.forEach(integerHeap::insert);
+
+        //        then
+        final List<Integer> expectedItems = Arrays.asList(2, 3, 4);
+        expectedItems.forEach(i -> assertThat(integerHeap.pop()).isEqualTo(i));
     }
 }
