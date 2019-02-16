d35ecf85e8891c83fa799c4b9fece4e5a2e765f8
htbkoo <h1017760@connect.hku.hk>
2019-02-17 01:21:37 +0800

    refactored BalancedBrackets.java htbkoo/java

--- java/src/main/java/online/hackerrank/interviewPreparationKit/Arrays/BalancedBrackets.java
+++ java/src/main/java/online/hackerrank/interviewPreparationKit/Arrays/BalancedBrackets.java
@@ -4,8 +4,11 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class BalancedBrackets {
     public static class Solution {
@@ -13,45 +16,46 @@ public class BalancedBrackets {
         private static final String YES = "YES";
         private static final String NO = "NO";
 
+        enum Bracket {
+            CURLY_OPEN("{", "}", true), ROUND_OPEN("(", ")", true), SQUARE_OPEN("[", "]", true),
+            CURLY_CLOSE("}", "{", false), ROUND_CLOSE(")", "(", false), SQUARE_CLOSE("]", "[", false);
+
+            private final String ch;
+            private final String corresponding;
+            private final boolean isOpen;
+
+            Bracket(String ch, String corresponding, boolean isOpen) {
+                this.ch = ch;
+                this.corresponding = corresponding;
+                this.isOpen = isOpen;
+            }
+
+            static Bracket from(String ch) {
+                return Arrays.stream(values())
+                        .filter(bracket -> bracket.ch.equals(ch))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException(String.format("Unknown character %s", ch)));
+            }
+        }
+
         // Complete the isBalanced function below.
         static String isBalanced(String s) {
-            Deque<String> brackets = new ArrayDeque<>();
+            Deque<Bracket> opened = new ArrayDeque<>();
+            final List<Bracket> brackets = Arrays.stream(s.split("")).map(Bracket::from).collect(Collectors.toList());
 
-            final String[] chars = s.split("");
-            for (String ch : chars) {
-                switch (ch) {
-                    case "{":
-                    case "(":
-                    case "[": {
-                        brackets.push(ch);
-                        break;
-                    }
-                    case ("}"): {
-                        if (isInvalid(brackets, "{")){
-                            return NO;
-                        }
-                        break;
-                    }
-                    case (")"): {
-                        if (isInvalid(brackets, "(")){
-                            return NO;
-                        }
-                        break;
-                    }
-                    case ("]"): {
-                        if (isInvalid(brackets, "[")){
-                            return NO;
-                        }
-                        break;
-                    }
+            for (Bracket b : brackets) {
+                if (b.isOpen) {
+                    opened.push(b);
+                } else if (isInvalid(opened, b)) {
+                    return NO;
                 }
             }
 
-            return brackets.isEmpty() ? YES : NO;
+            return opened.isEmpty() ? YES : NO;
         }
 
-        private static boolean isInvalid(Deque<String> brackets, String corresponding) {
-            return brackets.isEmpty() || !corresponding.equals(brackets.pop());
+        private static boolean isInvalid(Deque<Bracket> brackets, Bracket closeBracket) {
+            return brackets.isEmpty() || !closeBracket.corresponding.equals(brackets.pop().ch);
         }
 
         private static final Scanner scanner = new Scanner(System.in);
