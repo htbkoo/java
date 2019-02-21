262e50275390971fdfa1e7a850c47ba6556ca9cc
htbkoo <h1017760@connect.hku.hk>
2019-02-21 19:29:48 +0800

    updated `template/Main.java` htbkoo/java

--- java/src/main/java/online/atCoder/template/Main.java
+++ java/src/main/java/online/atCoder/template/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        TestCase testCase = readFromInput();
+
+        process(testCase);
+
+        output();
+    }
+
+    private static TestCase readFromInput() {
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+        return new TestCase();
+    }
+
+    public static Object process(TestCase testCase) {
+        return null;
+    }
+
+
+    private static void output() {
+        System.out.println();
+    }
+
+    public static class TestCase {
 
     }
 }
