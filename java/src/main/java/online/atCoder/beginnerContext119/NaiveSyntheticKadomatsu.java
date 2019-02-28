c28780d41380c699abdc17506181b7bd7a618e8f
htbkoo <h1017760@connect.hku.hk>
2019-02-28 16:49:49 +0800

    SyntheticKadomatsu - added much cleaner implementation after reading solution htbkoo/java

---
+++ java/src/main/java/online/atCoder/beginnerContext119/NaiveSyntheticKadomatsu.java
@@ -0,0 +1,164 @@
+package online.atCoder.beginnerContext119;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class NaiveSyntheticKadomatsu {
+    private static final int MP_CONSUMPTION_COMPOSITION_MAGIC = 10;
+
+    public static int process(TestCase testCase) {
+        int N = testCase.N;
+        int A = testCase.A;
+        int B = testCase.B;
+        int C = testCase.C;
+        List<Integer> l = testCase.l;
+
+        List<Integer> targets = Arrays.asList(A, B, C);
+
+        return generateConfigPermutations(N, targets.size())
+                .stream()
+                .filter(Config::isValid)
+                .map(config -> config.asMpNeeded(targets, l))
+                .mapToInt(i -> i)
+                .min()
+                .orElseThrow(() -> new IllegalStateException("Possibly programming bug, no valid config available"));
+    }
+
+    private static List<Config> generateConfigPermutations(int N, int numPossibleValues) {
+        return IntStream.range(0, (int) Math.pow(numPossibleValues + 1, N))
+                .boxed()
+                .map(i -> Config.fromInt(N, numPossibleValues, i))
+                .collect(toList());
+    }
+
+    private static class Config {
+        private final List<Integer> assignedBamboos;
+        private final int[] occurrences;
+        private final boolean isValid;
+
+        public static Config fromInt(int N, int numPossibleValues, Integer i) {
+            final String unpadded = toUnpaddedConfigString(numPossibleValues + 1, i);
+            final List<Integer> assignedBamboos = unpadded.chars()
+                    .map(Character::getNumericValue)
+                    .boxed()
+                    .collect(toList());
+            return new Config(assignedBamboos, numPossibleValues);
+        }
+
+        public boolean isValid() {
+            return isValid;
+        }
+
+        public int asMpNeeded(List<Integer> targets, List<Integer> l) {
+            return mpNeededForComposition() + mpNeededForLengthChanging(targets, l);
+        }
+
+        private Config(List<Integer> assignedBamboos, int numPossibleValues) {
+            final int[] occurrences = countOccurrences(assignedBamboos, numPossibleValues);
+            this.assignedBamboos = assignedBamboos;
+            this.occurrences = occurrences;
+            this.isValid = checkIsValid(occurrences, numPossibleValues);
+        }
+
+        private int mpNeededForComposition() {
+            int mpNeeded = 0;
+            for (int i = 1, numPossibleValues = occurrences.length; i < numPossibleValues; ++i) {
+                mpNeeded += MP_CONSUMPTION_COMPOSITION_MAGIC * (occurrences[i] - 1);
+            }
+            return mpNeeded;
+        }
+
+        private int mpNeededForLengthChanging(List<Integer> targets, List<Integer> l) {
+            int mpNeeded = 0;
+            for (int i = 1, numPossibleValues = occurrences.length; i < numPossibleValues; ++i) {
+                final int target = targets.get(i - 1);
+                int combinedBambooLength = getCombinedBambooLength(i, l);
+                mpNeeded += Math.abs(target - combinedBambooLength);
+            }
+            return mpNeeded;
+        }
+
+        private int getCombinedBambooLength(int i, List<Integer> l) {
+            int combinedBambooLength = 0;
+
+            final int numAssignedBamboos = assignedBamboos.size();
+            for (int j = numAssignedBamboos - 1; j >= 0; --j) {
+                if (i == assignedBamboos.get(j)) {
+                    final int pickedBambooLength = l.get(l.size() - 1 + j - (numAssignedBamboos - 1));
+                    combinedBambooLength += pickedBambooLength;
+                }
+            }
+            return combinedBambooLength;
+        }
+
+        private int[] countOccurrences(List<Integer> assignedBamboos, int numPossibleValues) {
+            final int[] occurrences = new int[numPossibleValues + 1];
+            assignedBamboos.forEach(i -> occurrences[i]++);
+            return occurrences;
+        }
+
+        private boolean checkIsValid(int[] occurrences, int numPossibleValues) {
+            for (int i = 1; i <= numPossibleValues; ++i) {
+                if (occurrences[i] < 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static String toUnpaddedConfigString(int N, Integer i) {
+            return Integer.toString(i, N);
+        }
+    }
+
+    // Util func
+
+    public static void main(String[] args) {
+        TestCase testCase = readFromInput();
+
+        final int result = process(testCase);
+
+        output(result);
+    }
+
+    private static TestCase readFromInput() {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < N; ++i) {
+            l.add(sc.nextInt());
+        }
+
+        sc.close();
+        return new TestCase(N, A, B, C, l);
+    }
+
+    private static void output(int result) {
+        System.out.println(result);
+    }
+
+    public static class TestCase {
+        final int N;
+        final int A;
+        final int B;
+        final int C;
+        final List<Integer> l;
+
+        public TestCase(int N, int A, int B, int C, final List<Integer> l) {
+            this.N = N;
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.l = l;
+        }
+    }
+}
--- java/src/main/java/online/atCoder/beginnerContext119/SyntheticKadomatsu.java
+++ java/src/main/java/online/atCoder/beginnerContext119/SyntheticKadomatsu.java
@@ -1,119 +1,43 @@
 package online.atCoder.beginnerContext119;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
 
 public class SyntheticKadomatsu {
     private static final int MP_CONSUMPTION_COMPOSITION_MAGIC = 10;
+    private static final int IMPOSSIBLE = 10_000_000;
 
     public static int process(TestCase testCase) {
+        return dfs(testCase, 0, 0, 0, 0);
+    }
+
+    private static int dfs(TestCase testCase, int curr, int a, int b, int c) {
         int N = testCase.N;
         int A = testCase.A;
         int B = testCase.B;
         int C = testCase.C;
-        List<Integer> l = testCase.l;
 
-        List<Integer> targets = Arrays.asList(A, B, C);
+        if (curr == N) {
+            if (a > 0 && b > 0 && c > 0) {
+                return diff(a, A) + diff(b, B) + diff(c, C) - 3 * MP_CONSUMPTION_COMPOSITION_MAGIC;
+            } else {
+                return IMPOSSIBLE;
+            }
+        } else {
+            List<Integer> l = testCase.l;
+            final int value = l.get(curr);
+            int v0 = dfs(testCase, curr + 1, a, b, c);
+            int v1 = dfs(testCase, curr + 1, a + value, b, c) + MP_CONSUMPTION_COMPOSITION_MAGIC;
+            int v2 = dfs(testCase, curr + 1, a, b + value, c) + MP_CONSUMPTION_COMPOSITION_MAGIC;
+            int v3 = dfs(testCase, curr + 1, a, b, c + value) + MP_CONSUMPTION_COMPOSITION_MAGIC;
 
-        return generateConfigPermutations(N, targets.size())
-                .stream()
-                .filter(Config::isValid)
-                .map(config -> config.asMpNeeded(targets, l))
-                .mapToInt(i -> i)
-                .min()
-                .orElseThrow(() -> new IllegalStateException("Possibly programming bug, no valid config available"));
+            return Math.min(Math.min(Math.min(v0, v1), v2), v3);
+        }
     }
 
-    private static List<Config> generateConfigPermutations(int N, int numPossibleValues) {
-        return IntStream.range(0, (int) Math.pow(numPossibleValues + 1, N))
-                .boxed()
-                .map(i -> Config.fromInt(N, numPossibleValues, i))
-                .collect(toList());
-    }
-
-    private static class Config {
-        private final List<Integer> assignedBamboos;
-        private final int[] occurrences;
-        private final boolean isValid;
-
-        public static Config fromInt(int N, int numPossibleValues, Integer i) {
-            final String unpadded = toUnpaddedConfigString(numPossibleValues + 1, i);
-            final List<Integer> assignedBamboos = unpadded.chars()
-                    .map(Character::getNumericValue)
-                    .boxed()
-                    .collect(toList());
-            return new Config(assignedBamboos, numPossibleValues);
-        }
-
-        public boolean isValid() {
-            return isValid;
-        }
-
-        public int asMpNeeded(List<Integer> targets, List<Integer> l) {
-            return mpNeededForComposition() + mpNeededForLengthChanging(targets, l);
-        }
-
-        private Config(List<Integer> assignedBamboos, int numPossibleValues) {
-            final int[] occurrences = countOccurrences(assignedBamboos, numPossibleValues);
-            this.assignedBamboos = assignedBamboos;
-            this.occurrences = occurrences;
-            this.isValid = checkIsValid(occurrences, numPossibleValues);
-        }
-
-        private int mpNeededForComposition() {
-            int mpNeeded = 0;
-            for (int i = 1, numPossibleValues = occurrences.length; i < numPossibleValues; ++i) {
-                mpNeeded += MP_CONSUMPTION_COMPOSITION_MAGIC * (occurrences[i] - 1);
-            }
-            return mpNeeded;
-        }
-
-        private int mpNeededForLengthChanging(List<Integer> targets, List<Integer> l) {
-            int mpNeeded = 0;
-            for (int i = 1, numPossibleValues = occurrences.length; i < numPossibleValues; ++i) {
-                final int target = targets.get(i - 1);
-                int combinedBambooLength = getCombinedBambooLength(i, l);
-                mpNeeded += Math.abs(target - combinedBambooLength);
-            }
-            return mpNeeded;
-        }
-
-        private int getCombinedBambooLength(int i, List<Integer> l) {
-            int combinedBambooLength = 0;
-
-            final int numAssignedBamboos = assignedBamboos.size();
-            for (int j = numAssignedBamboos - 1; j >= 0; --j) {
-                if (i == assignedBamboos.get(j)) {
-                    final int pickedBambooLength = l.get(l.size() - 1 + j - (numAssignedBamboos - 1));
-                    combinedBambooLength += pickedBambooLength;
-                }
-            }
-            return combinedBambooLength;
-        }
-
-        private int[] countOccurrences(List<Integer> assignedBamboos, int numPossibleValues) {
-            final int[] occurrences = new int[numPossibleValues + 1];
-            assignedBamboos.forEach(i -> occurrences[i]++);
-            return occurrences;
-        }
-
-        private boolean checkIsValid(int[] occurrences, int numPossibleValues) {
-            for (int i = 1; i <= numPossibleValues; ++i) {
-                if (occurrences[i] < 1) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private static String toUnpaddedConfigString(int N, Integer i) {
-            return Integer.toString(i, N);
-        }
+    private static int diff(int actual, int target) {
+        return Math.abs(actual - target);
     }
 
     // Util func
