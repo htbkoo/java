344d84778ceaa15a1b276e0ecec02e4dd79fe7c9
htbkoo <h1017760@connect.hku.hk>
2019-03-02 13:13:27 +0800

    (WIP) LinearApproximation.java htbkoo/java

--- java/src/main/java/online/atCoder/regularContext100/LinearApproximation.java
+++ java/src/main/java/online/atCoder/regularContext100/LinearApproximation.java
@@ -1,6 +1,7 @@
 package online.atCoder.regularContext100;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,57 +11,25 @@ public class LinearApproximation {
         List<Integer> A = testCase.A;
 
         List<Integer> differences = new ArrayList<>();
-        List<Integer> negative = new ArrayList<>();
-        List<Integer> positive = new ArrayList<>();
-        int zeroCount = 0;
 
         for (int i = 0; i < N; ++i) {
             int target = (i + 1);
             final int diff = target - A.get(i);
             differences.add(diff);
-
-            if (diff == 0) {
-                zeroCount++;
-            } else if (diff < 0) {
-                negative.add(diff);
-            } else {
-                positive.add(diff);
-            }
         }
 
         differences.sort(Integer::compareTo);
 
         int mid = N / 2;
         final int midDiff = differences.get(mid);
-        final int midPlusOneDiff = differences.get(mid + 1);
-        if (noChangeNeeded(N, positive, negative, midDiff, midPlusOneDiff)) {
-            return getAbsSum(negative) + getAbsSum(positive);
-        } else {
-            return getResult(zeroCount, negative, positive, midDiff);
-        }
+
+        return sadness(differences, midDiff);
     }
 
-    private static int getResult(int zeroCount, List<Integer> negative, List<Integer> positive, int midDiff) {
-        int negativeSum = Math.abs(sum(negative) - midDiff * negative.size());
-        int positiveSum = Math.abs(sum(positive) - midDiff * positive.size());
-        int zeroSum = Math.abs(zeroCount * midDiff);
-        return negativeSum + zeroSum + positiveSum;
-    }
-
-    private static boolean noChangeNeeded(int N, List<Integer> positive, List<Integer> negative, int midDiff, int midPlusOneDiff) {
-        final boolean isMidDiffZero = midDiff == 0;
-        final boolean isForEvenMidPlusOneDiffZero = (N % 2 == 0) && midPlusOneDiff == 0;
-        final boolean isSameNumPosAndNeg = positive.size() == negative.size();
-
-        return isMidDiffZero || isForEvenMidPlusOneDiffZero || isSameNumPosAndNeg;
-    }
-
-    private static int getAbsSum(List<Integer> list) {
-        return Math.abs(sum(list));
-    }
-
-    private static int sum(List<Integer> list) {
-        return list.stream().mapToInt(i -> i).sum();
+    private static int sadness(Collection<Integer> differences, int midDiff) {
+        return differences.stream()
+                .mapToInt(i -> Math.abs(i - midDiff))
+                .sum();
     }
 
     // Util func
--- java/src/test/groovy/online/atCoder/regularContext100/LinearApproximationTest.groovy
+++ java/src/test/groovy/online/atCoder/regularContext100/LinearApproximationTest.groovy
@@ -7,16 +7,22 @@ class LinearApproximationTest extends Specification {
     @Unroll
     def "should find"() {
         when:
-        def result = LinearApproximation.process(new LinearApproximation.TestCase(N,A))
+        def result = LinearApproximation.process(new LinearApproximation.TestCase(N, A))
 
         then:
         result == expected
 
         where:
         N || A                           || expected
+        1 || [10]                        || 0
+        5 || [-4, -3, -2, -1, 0]         || 0
+        5 || [0, 1, 2, 3, 4]             || 0
         5 || [2, 2, 3, 5, 5]             || 2
+        5 || [2, 3, 4, 5, 6]             || 0
+        5 || [2, 3, 7, 5, 7]             || 4
         9 || [1, 2, 3, 4, 5, 6, 7, 8, 9] || 0
         6 || [6, 5, 4, 3, 2, 1]          || 18
+        6 || [1, 4, 1, 5, 1, 6]          || 9
         7 || [1, 1, 1, 1, 2, 3, 4]       || 6
     }
 
