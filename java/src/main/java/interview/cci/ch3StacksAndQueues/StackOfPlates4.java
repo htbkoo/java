90e4dd7ee2a8f9f49ffb14d0cf7037a77a2b6e2a
htbkoo <h1017760@connect.hku.hk>
2019-03-05 21:29:30 +0800

    created StackOfPlates4.java htbkoo/java

--- java/src/main/java/interview/cci/ch3StacksAndQueues/StackOfPlates4.java
+++ java/src/main/java/interview/cci/ch3StacksAndQueues/StackOfPlates4.java
@@ -1,32 +1,65 @@
 package interview.cci.ch3StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StackOfPlates4 {
     public static class SetOfStacks<T> {
         private final int capacity;
+        private final Deque<Deque<T>> stacks;
 
         public SetOfStacks(int capacity) {
             this.capacity = capacity;
+            stacks = new ArrayDeque<>();
         }
 
         public void push(T item) {
+            if (isEmpty() || isTopStackFull()) {
+                stacks.push(new ArrayDeque<>());
+            }
+            // Impossible, new deque would be created if stacks is empty
+            //noinspection ConstantConditions
+            stacks.peek().push(item);
+        }
 
+        private boolean isTopStackFull() {
+            if (stacks.isEmpty()) {
+                throw new IllegalStateException("Defect: stack is empty when calling isTopStackFull()");
+            } else {
+                return stacks.peek().size() >= capacity;
+            }
         }
 
         public T pop() {
-            return null;
+            if (isEmpty()) {
+                throw new IllegalStateException("Stack is empty");
+            } else {
+                final Deque<T> topStack = stacks.peek();
+                // Impossible, already check stacks is not empty
+                //noinspection ConstantConditions
+                if (topStack.size() <= 1) {
+                    stacks.pop();
+                }
+                return topStack.pop();
+            }
         }
 
         public T peek() {
-            return null;
-
+            if (isEmpty()) {
+                throw new IllegalStateException("Stack is empty");
+            } else {
+                // Impossible, already check stacks is not empty
+                //noinspection ConstantConditions
+                return stacks.peek().peek();
+            }
         }
 
         public boolean isEmpty() {
-            return false;
+            return stacks.isEmpty();
         }
 
         public int numStacks() {
-            return 0;
+            return stacks.size();
         }
     }
 }
