338435fcd5715f6b1da2e6519d442201f7552ee5
htbkoo <h1017760@connect.hku.hk>
2019-03-07 00:01:24 +0800

    created ValidateBST5.java version 4 htbkoo/java

--- java/src/main/java/interview/cci/ch4TreesAndGraphs/ValidateBST5.java
+++ java/src/main/java/interview/cci/ch4TreesAndGraphs/ValidateBST5.java
@@ -2,6 +2,11 @@ package interview.cci.ch4TreesAndGraphs;
 
 import interview.cci.ch4TreesAndGraphs.util.TreeNode;
 
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,25 +19,26 @@ import interview.cci.ch4TreesAndGraphs.util.TreeNode;
 
 public class ValidateBST5 {
     private static final boolean ASSUME_EMPTY_TREE_IS_VALID = true;
+    private static final boolean UNBOUNDED_IS_ALWAYS_VALID = true;
 
     public boolean isValid(TreeNode root) {
         if (root == null) {
             return ASSUME_EMPTY_TREE_IS_VALID;
         } else {
-            return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+            return isValid(root, empty(), empty());
         }
     }
 
-    private boolean isValid(TreeNode node, long lo, long hi) {
+    private boolean isValid(TreeNode node, Optional<Integer> lo, Optional<Integer> hi) {
         if (node == null) {
             return true;
         } else {
             final int val = node.val;
-            return isWithInRange(val, lo, hi) && isValid(node.left, lo, val) && isValid(node.right, val, hi);
+            return isWithInRange(val, lo, hi) && isValid(node.left, lo, of(val)) && isValid(node.right, of(val), hi);
         }
     }
 
-    private boolean isWithInRange(int val, long lo, long hi) {
-        return lo < val && val < hi;
+    private boolean isWithInRange(int val, Optional<Integer> optionalLo, Optional<Integer> optionalHi) {
+        return optionalLo.map(lo -> lo < val).orElse(UNBOUNDED_IS_ALWAYS_VALID) && optionalHi.map(hi -> val < hi).orElse(UNBOUNDED_IS_ALWAYS_VALID);
     }
 }
