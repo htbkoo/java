59dbdac6708df1a10086ba8d10ebed73eeef4403
htbkoo <h1017760@connect.hku.hk>
2019-03-06 23:52:45 +0800

    created ValidateBST5.java version 2 htbkoo/java

--- java/src/main/java/interview/cci/ch4TreesAndGraphs/ValidateBST5.java
+++ java/src/main/java/interview/cci/ch4TreesAndGraphs/ValidateBST5.java
@@ -33,6 +33,6 @@ public class ValidateBST5 {
     }
 
     private boolean isWithInRange(int val, int lo, int hi) {
-        return lo <= val && val <= hi;
+        return lo < val && val < hi;
     }
 }
