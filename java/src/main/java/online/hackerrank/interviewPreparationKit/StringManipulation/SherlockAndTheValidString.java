46548f7b5d4fffba72042d08ffa7a461471bbb1c
htbkoo <h1017760@connect.hku.hk>
2019-03-18 09:06:34 +0800

    cleaned up SherlockAndTheValidString.java htbkoo/java

--- java/src/main/java/online/hackerrank/interviewPreparationKit/StringManipulation/SherlockAndTheValidString.java
+++ java/src/main/java/online/hackerrank/interviewPreparationKit/StringManipulation/SherlockAndTheValidString.java
@@ -3,14 +3,16 @@ package online.hackerrank.interviewPreparationKit.StringManipulation;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class SherlockAndTheValidString {
     public static class Solution {
@@ -18,6 +20,8 @@ public class SherlockAndTheValidString {
         private static final int ASCII_RANGE = 256;
         private static final String YES = "YES";
         private static final String NO = "NO";
+        private static final Function<Integer, Integer> START_COUNT_AT_ONE = i -> 1;
+        private static final BinaryOperator<Integer> COMBINE_COUNTS_FOR_SAME_KEY = (i, j) -> i + j;
 
         // Complete the isValid function below.
         static String isValid(String s) {
@@ -47,25 +51,19 @@ public class SherlockAndTheValidString {
 
         // precondition: there are only 2 kind of number in frequencies
         private static boolean isValidByRemovingOneCharacter(List<Integer> frequencies) {
-            Map<Integer, Integer> reversedIndices = new HashMap<>();
-            frequencies.forEach(f -> {
-                if (reversedIndices.containsKey(f)) {
-                    reversedIndices.put(f, reversedIndices.get(f) + 1);
-                } else {
-                    reversedIndices.put(f, 1);
-                }
-            });
+            Map<Integer, Integer> reversedIndices = frequencies.stream().collect(toMap(i -> i, START_COUNT_AT_ONE, COMBINE_COUNTS_FOR_SAME_KEY));
 
-            if (reversedIndices.containsKey(1) && reversedIndices.get(1) == 1) {
+            boolean isOnlyOneRedundantCharacter = reversedIndices.containsKey(1) && reversedIndices.get(1) == 1;
+            if (isOnlyOneRedundantCharacter) {
                 return true;
+            } else {
+                int smallerKey = reversedIndices.keySet().stream().mapToInt(i -> i).min().orElseThrow(() -> new IllegalStateException("reversedIndices is empty"));
+                int largerKey = reversedIndices.keySet().stream().mapToInt(i -> i).max().orElseThrow(() -> new IllegalStateException("reversedIndices is empty"));
+
+                boolean isFrequencyDifferByOne = (largerKey - smallerKey) <= 1;
+                boolean isOnlyOneCharacterAppearsMore = reversedIndices.get(largerKey) == 1;
+                return isFrequencyDifferByOne && isOnlyOneCharacterAppearsMore;
             }
-
-            int smallerKey = reversedIndices.keySet().stream().mapToInt(i -> i).min().orElseThrow(() -> new IllegalStateException("reversedIndices is empty"));
-            int largerKey = reversedIndices.keySet().stream().mapToInt(i -> i).max().orElseThrow(() -> new IllegalStateException("reversedIndices is empty"));
-
-            boolean isFrequencyDifferByOne = (largerKey - smallerKey) <= 1;
-            boolean isOnlyOneCharacterAppearsMore = reversedIndices.get(largerKey) == 1;
-            return isFrequencyDifferByOne && isOnlyOneCharacterAppearsMore;
         }
 
         private static final Scanner scanner = new Scanner(System.in);
