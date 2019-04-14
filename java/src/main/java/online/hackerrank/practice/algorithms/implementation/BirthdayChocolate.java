bc0edbf83a056fe82ab0d22297f29d3b7ba27caa
htbkoo <h1017760@connect.hku.hk>
2019-04-14 14:32:10 +0800

    solved BirthdayChocolate.java

--- java/src/main/java/online/hackerrank/practice/algorithms/implementation/BirthdayChocolate.java
+++ java/src/main/java/online/hackerrank/practice/algorithms/implementation/BirthdayChocolate.java
@@ -5,73 +5,24 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.IntStream.range;
 
 public class BirthdayChocolate {
     public static class Solution {
-        private static class State {
-            static final State EMPTY = new State(Collections.emptyList());
-            private final List<Set<Integer>> configurations;
-
-            private State(List<Set<Integer>> configurations) {
-                this.configurations = configurations;
-            }
-
-            static boolean isNewStateValid(List<Integer> s, int newIndex) {
-                return false;
-            }
-
-            int getNumWays() {
-                return configurations.size();
-            }
-        }
-
         // Complete the birthday function below.
         static int birthday(List<Integer> s, int d, int m) {
-            int n = s.size();
-            int[][][] states = initialStates(d, m, n);
+            // to circumvent Java's stream API constraint :(
+            int sum = s.subList(0, m).stream().mapToInt(i -> i).sum();
+            int count = (sum == d) ? 1 : 0;
+            for (int i = m, n = s.size(); i < n; i++) {
+                sum = sum + s.get(i) - s.get(i - m);
+                count += (sum == d) ? 1 : 0;
+            }
 
-            range(1, m + 1).forEach(i ->
-                    range(1, d + 1).forEach(j ->
-                            range(1, n + 1).forEach(k -> {
-                                        Integer value = s.get(k - 1);
-                                        int newNumWays = states[i][j][k - 1] + (((j - value) >= 0) ? states[i - 1][j - value][k - 1] : 0);
-                                        states[i][j][k] = newNumWays;
-                                    }
-                            )
-                    )
-            );
-
-            return states[m][d][n];
-        }
-
-        private static int[][][] initialStates(int d, int m, int n) {
-            int[][][] states = new int[m + 1][d + 1][n + 1];
-
-            // states[0][0][:] all have 1 possible configuration which is the empty set (i.e. picking no chocolate)
-//            range(0, d + 1).forEach(j ->
-            range(0, n + 1).forEach(k ->
-                            states[0][0][k] = 1
-//                    )
-            );
-
-            // states[1:][:][0] and states[1:][0][:] have no possible configurations because 1<=s[i]<=5
-            range(1, m + 1).forEach(i -> {
-                        range(0, d + 1).forEach(j ->
-                                states[i][j][0] = 0
-                        );
-                        range(0, n + 1).forEach(k ->
-                                states[i][0][k] = 0
-                        );
-                    }
-            );
-            return states;
+            return count;
         }
 
         public static void main(String[] args) throws IOException {
