13de8fd210b27dd520a56a0f5bd280c3cce35a52
htbkoo <h1017760@connect.hku.hk>
2019-04-20 11:28:25 +0800

    added Unroll annotation

--- java/src/test/groovy/online/hackerrank/practice/dataStructures/stacks/EqualStacksTest.groovy
+++ java/src/test/groovy/online/hackerrank/practice/dataStructures/stacks/EqualStacksTest.groovy
@@ -1,8 +1,10 @@
 package online.hackerrank.practice.dataStructures.stacks
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class EqualStacksTest extends Specification {
+    @Unroll
     def "test"() {
         when:
         def actual = EqualStacks.Solution.equalStacks(h1 as int[], h2 as int[], h3 as int[])
