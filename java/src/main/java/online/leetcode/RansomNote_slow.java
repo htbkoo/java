c0adb86342f7add86c8e7404c65d7fe68b550761
htbkoo <h1017760@connect.hku.hk>
2019-04-25 15:54:31 +0800

    marked first submission for RansomNote.java as slow

---
+++ java/src/main/java/online/leetcode/RansomNote_slow.java
@@ -0,0 +1,45 @@
+package online.leetcode;
+
+/*
+
+https://leetcode.com/problems/ransom-note/
+
+383. Ransom Note
+
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+*/
+
+public class RansomNote_slow {
+    static class Solution {
+        private static final int ASCII_RANGE = 256;
+
+        public boolean canConstruct(String ransomNote, String magazine) {
+            int[] ransomNoteFrequencies = countFrequency(ransomNote);
+            int[] magazineFrequencies = countFrequency(magazine);
+
+            for (int i = 0; i < ransomNoteFrequencies.length; i++) {
+                if (ransomNoteFrequencies[i]>magazineFrequencies[i]){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int[] countFrequency(String str) {
+            int[] frequencies = new int[ASCII_RANGE];
+            str.chars().forEach(charCode -> frequencies[charCode]++);
+            return frequencies;
+        }
+    }
+}
