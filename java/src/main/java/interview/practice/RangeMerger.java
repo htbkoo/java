2f95be2e76fd479986851f36c5a9d044b0823c15
htbkoo <h1017760@connect.hku.hk>
2019-05-01 18:32:58 +0800

    implemented first attempt for RangeMerger.java

--- java/src/main/java/interview/practice/RangeMerger.java
+++ java/src/main/java/interview/practice/RangeMerger.java
@@ -1,20 +1,102 @@
 package interview.practice;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Objects;
+import java.util.TreeSet;
+
+import static java.util.Comparator.comparing;
 
 public class RangeMerger {
-    Set<Long> set = new HashSet<>();
+    private static class Range {
+        final long start;
+        final long end;
 
-    public void put(long id) {
-        set.add(id);
+        Range(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        static Range fromId(long id) {
+            return new Range(id, id);
+        }
+
+        public long getStart() {
+            return start;
+        }
+
+        public long getEnd() {
+            return end;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Range range = (Range) o;
+            return start == range.start &&
+                    end == range.end;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, end);
+        }
     }
 
-    public boolean contains(long id) {
-        return set.contains(id);
+    private TreeSet<Range> ranges;
+
+    public RangeMerger() {
+        this.ranges = createRangesTreeSet();
+    }
+
+    private TreeSet<Range> createRangesTreeSet() {
+        return new TreeSet<>(comparing(Range::getStart));
+//        return new TreeSet<>(Comparator.comparing(Range::getStart).thenComparing(Range::getEnd));
+    }
+
+    public void put(long id) {
+        Range idAsRange = Range.fromId(id);
+        if (!contains(idAsRange)) {
+            ranges.add(idAsRange);
+            this.ranges = mergeRanges();
+        }
     }
 
     public int size() {
-        return set.size();
+        return ranges.size();
+    }
+
+    public boolean contains(long id) {
+        return contains(Range.fromId(id));
+    }
+
+    private TreeSet<Range> mergeRanges() {
+        final TreeSet<Range> newRanges = createRangesTreeSet();
+        if (!this.ranges.isEmpty()) {
+            long currentStart = ranges.first().start, currentEnd = ranges.first().end;
+            for (Range range : ranges) {
+                if (canMerge(currentEnd, range)) {
+                    currentEnd = range.end;
+                } else {
+                    newRanges.add(new Range(currentStart, currentEnd));
+                    currentStart = range.start;
+                    currentEnd = range.end;
+                }
+            }
+            newRanges.add(new Range(currentStart, currentEnd));
+        }
+        return newRanges;
+    }
+
+    private boolean canMerge(long currentEnd, Range range) {
+        return currentEnd >= (range.start - 1);
+    }
+
+    private boolean contains(Range idAsRange) {
+        Range possiblyPrevRange = ranges.floor(idAsRange);
+        if (possiblyPrevRange != null) {
+            return possiblyPrevRange.start <= idAsRange.start && idAsRange.end <= possiblyPrevRange.end;
+        } else {
+            return false;
+        }
     }
 }
--- java/src/test/groovy/interview/practice/RangeMergerTest.groovy
+++ java/src/test/groovy/interview/practice/RangeMergerTest.groovy
@@ -6,19 +6,27 @@ import spock.lang.Unroll
 class RangeMergerTest extends Specification {
     @Unroll
     def "should manage ranges"() {
-        given:
-        def ids = [1]
+        when:
         def merger = new RangeMerger()
 
-        when:
-        ids.each {
-            merger.put(it)
-            merger.contains(it)
+        then:
+        operations.each {
+            merger.put(it.id)
+            assert merger.contains(it.id)
+            assert merger.size() == it.expectedSize
         }
 
-        then:
-        def expectedSize = 1
-        ids.every { merger.contains(it) }
-        merger.size() == expectedSize
+        where:
+        operations << [
+                [op(1, 1),],
+                [op(1, 1), op(2, 1),],
+                [op(1, 1), op(3, 2),],
+                [op(1, 1), op(3, 2), op(2, 1)],
+                [op(1, 1), op(3, 2), op(4, 2), op(2, 1)],
+        ]
+    }
+
+    def op(int id, int size) {
+        [id: id, expectedSize: size]
     }
 }
