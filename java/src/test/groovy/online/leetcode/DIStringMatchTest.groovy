6df3bee77b8ccce4e9c9f9548b9735c7dfc71e1f
htbkoo <h1017760@connect.hku.hk>
2019-05-07 14:18:59 +0800

    solved DIStringMatch.java

---
+++ java/src/test/groovy/online/leetcode/DIStringMatchTest.groovy
@@ -0,0 +1,22 @@
+package online.leetcode
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DIStringMatchTest extends Specification {
+
+    @Unroll
+    def "test"() {
+        when:
+        def actual = new DIStringMatch.Solution().diStringMatch(S)
+
+        then:
+        actual == expected as int[]
+
+        where:
+        S      || expected
+        "IDID" || [0, 4, 1, 3, 2]
+        "III"  || [0, 1, 2, 3]
+        "DDI"  || [3, 2, 0, 1]
+    }
+}
