310caae36ba1ce77b92a1b96e9eb219f8484cd9f
htbkoo <h1017760@connect.hku.hk>
2019-05-07 19:58:23 +0800

    solved Fibonacci.java

--- java/src/main/java/online/leetcode/Fibonacci.java
+++ java/src/main/java/online/leetcode/Fibonacci.java
@@ -2,44 +2,40 @@ package online.leetcode;
 
 /*
 
-https://leetcode.com/problems/distribute-coins-in-binary-tree/
+https://leetcode.com/problems/fibonacci-number/
 
-Given the root of a binary tree with N nodes, each node in the tree has node.val coins, and there are N coins total.
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 
-In one move, we may choose two adjacent nodes and move one coin from one node to another.  (The move may be from parent to child, or from child to parent.)
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
 
-Return the number of moves required to make every node have exactly one coin.
+Given N, calculate F(N).
 
 
 
 Example 1:
 
-Input: [3,0,0]
-Output: 2
-Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+Input: 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
 
 Example 2:
 
-Input: [0,3,0]
-Output: 3
-Explanation: From the left child of the root, we move two coins to the root [taking two moves].  Then, we move one coin from the root of the tree to the right child.
+Input: 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
 
 Example 3:
 
-Input: [1,0,2]
-Output: 2
-
-Example 4:
-
-Input: [1,0,0,null,3]
-Output: 4
+Input: 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
 
 
 Note:
 
-    1<= N <= 100
-    0 <= node.val <= N
+0 ≤ N ≤ 30.
 
 */
 
