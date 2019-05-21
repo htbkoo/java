30d37c2ffea89212a445ea6e1ef2d1f467fe37bd
htbkoo <h1017760@connect.hku.hk>
2019-05-21 19:51:34 +0800

    refactored PowerSet4.java

--- java/src/main/java/interview/cci/ch8RecursionAndDynamicProgramming/PowerSet4.java
+++ java/src/main/java/interview/cci/ch8RecursionAndDynamicProgramming/PowerSet4.java
@@ -5,25 +5,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PowerSet4 {
-
     public Set<Set<Integer>> findAllSubsets(Set<Integer> set) {
-        return subsets(new HashSet<>(), set);
+        Set<Set<Integer>> answers = new HashSet<>();
+        answers.add(new HashSet<>());
+        for (Integer i: set){
+            Set<Set<Integer>> newRow = new HashSet<>();
+            for (Set<Integer> answer: answers){
+                Set<Integer> newAnswer = new HashSet<>(answer);
+                newAnswer.add(i);
+                newRow.add(newAnswer);
+            }
+            answers.addAll(newRow);
+        }
+        return answers;
     }
 
-    private Set<Set<Integer>> subsets(Set<Integer> current, Set<Integer> candidates) {
-        if (candidates.isEmpty()) {
-            return new HashSet<>(Collections.singletonList(current));
-        } else {
-            Set<Set<Integer>> answers = new HashSet<>();
-            answers.add(current);
-            candidates.forEach(candidate -> {
-                Set<Integer> newCurrent = new HashSet<>(current);
-                newCurrent.add(candidate);
-                Set<Integer> newCandidates = new HashSet<>(candidates);
-                newCandidates.remove(candidate);
-                answers.addAll(subsets(newCurrent, newCandidates));
-            });
-            return answers;
+    private class PoorApproachWithDuplicate{
+        public Set<Set<Integer>> findAllSubsets(Set<Integer> set) {
+            return subsets(new HashSet<>(), set);
+        }
+
+        private Set<Set<Integer>> subsets(Set<Integer> current, Set<Integer> candidates) {
+            if (candidates.isEmpty()) {
+                return new HashSet<>(Collections.singletonList(current));
+            } else {
+                Set<Set<Integer>> answers = new HashSet<>();
+                answers.add(current);
+                candidates.forEach(candidate -> {
+                    Set<Integer> newCurrent = new HashSet<>(current);
+                    newCurrent.add(candidate);
+                    Set<Integer> newCandidates = new HashSet<>(candidates);
+                    newCandidates.remove(candidate);
+                    answers.addAll(subsets(newCurrent, newCandidates));
+                });
+                return answers;
+            }
         }
     }
 }
