ace3e0c5948fa8b7ef0344ec5ba59c133c63aca5
htbkoo <h1017760@connect.hku.hk>
2019-05-26 14:58:32 +0800

    solved UniquePathsII.java with slightly cleaner code

--- java/src/main/java/online/leetcode/UniquePathsII_unclean.java
+++ java/src/main/java/online/leetcode/UniquePathsII_unclean.java
@@ -31,26 +31,41 @@ There are two ways to reach the bottom-right corner:
 
 */
 
-public class UniquePathsII {
+public class UniquePathsII_unclean {
     static class Solution {
         public int uniquePathsWithObstacles(int[][] obstacleGrid) {
             if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
                 return 0;
             } else {
                 int height = obstacleGrid.length, width = obstacleGrid[0].length;
+                int[] currentRow;
+                int[] nextRow = new int[width];
 
-                int[] answers = new int[width];
-                answers[0]=1;
+                nextRow[0] = isBlocked(obstacleGrid, 0, 0) ? 0 : 1;
+                for (int j = 1; j < width; ++j) {
+                    nextRow[j] = isBlocked(obstacleGrid, 0, j) ? 0 : Math.min(1, nextRow[j - 1]);
+                }
 
-                for (int i=0;i<height;++i){
-                    answers[0] = (obstacleGrid[i][0]==1)?0:Math.min(1, answers[0]);
-                    for (int j=1;j<width;++j){
-                        answers[j] = (obstacleGrid[i][j]==1)?0:(answers[j]+answers[j-1]);
+                for (int i = 1; i < height; ++i) {
+                    currentRow = nextRow;
+                    nextRow = new int[width];
+
+                    nextRow[0] = isBlocked(obstacleGrid, i, 0) ? 0 : Math.min(1, currentRow[0]);
+
+                    for (int j = 1; j < width; ++j) {
+                        nextRow[j] = isBlocked(obstacleGrid, i, j)
+                                ? 0
+                                : (nextRow[j - 1] + currentRow[j]);
                     }
                 }
 
-                return answers[width-1];
+                return nextRow[width - 1];
             }
         }
+
+        private boolean isBlocked(int[][] obstacleGrid, int i, int j) {
+            return obstacleGrid[i][j] == 1;
+        }
+
     }
 }
