4ee2b5be4b9a7b4a3585df7fcb43bf0440200a75
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-05-04 21:54:07 +0200

    feat(Codewars): Add another solution for `_7kyu/ListFiltering.kt`

--- kotlin/src/main/kotlin/online/codewars/_7kyu/ListFiltering.kt
+++ kotlin/src/main/kotlin/online/codewars/_7kyu/ListFiltering.kt
@@ -1,6 +1,13 @@
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/kotlin
+
 package com.heydev.java.online.codewars._7kyu
 
 fun filterList(l: List<Any>): List<Int> {
+    // Your answer
+    return l.filterIsInstance<Int>()
+}
+
+fun firstFilterList(l: List<Any>): List<Int> {
     // Your answer
     return l.filter { it is Int } as List<Int>
 }
